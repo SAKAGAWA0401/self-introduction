dc5de125606d0a338e84da5124b6c624462c56b0
SAKAGAWA0401 <rts0401@gmail.com>
2025-01-09 02:37:59 +0800

    handle the 404 error on the portfolio page

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import Portfolio from './pages/Portfolio';
 
 function App() {
   return (
-    <BrowserRouter>
+    <Router basename="/self-introduction">
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/portfolio" element={<Portfolio />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
 
