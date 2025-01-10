54f8cf2e9e61c1641bed201a8080368114dcd3fb
SAKAGAWA0401 <rts0401@gmail.com>
2025-01-10 14:02:37 +0800

    added screenshots in public folder and tech stack on portfolio page Related #11

--- src/pages/Portfolio.tsx
+++ src/pages/Portfolio.tsx
@@ -21,12 +21,12 @@ const Portfolio = () => {
         <section className="bg-white/10 backdrop-blur-sm rounded-lg p-6">
           <h2 className="text-3xl font-bold mb-4">Operating Now</h2>
           <div className="space-y-4">
-            <div className="p-4 bg-white/5 rounded-lg">
-            <h3 className="text-xl font-semibold">Homepage/Portfolio (This site)</h3>
+            <div className="p-4 bg-white/5 rounded-lg space-y-2">
+              <h3 className="text-xl font-semibold">Homepage/Portfolio (This site)</h3>
               <div className="flex flex-col md:flex-row gap-4 items-center">
                 <div className="w-full md:w-1/3">
                   <img 
-                    src="https://placehold.co/400x200" 
+                    src="screenshot\screenshot-sloperiver-1280x720.png" 
                     alt="Homepage/Portfolio (This site)" 
                     className="rounded-lg cursor-pointer"
                     onClick={() => window.open('https://sloperiver.com', '_blank')}
@@ -41,21 +41,54 @@ const Portfolio = () => {
                     I'm proud to have built this entire site with a focus on the frontend, 
                     allowing me to host and maintain it completely free of charge.`}
                   </p>
-                  <div className="mt-4 flex flex-wrap gap-2">
-                    <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
-                    <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
-                    <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">Tailwind</span>
-                  </div>
                 </div>
               </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Languages & Frameworks:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
+                  <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
+                  <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">TailwindCSS</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Build Tool:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">Vite</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Deployment & Hosting:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">GitHub Pages</span>
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">GitHub Actions</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">others:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">CookieYes</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">GA4</span>                
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">bolt.new</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">cline(Deepseek v3)</span>
+                </div>
+              </div>              
             </div>
 
-            <div className="p-4 bg-white/5 rounded-lg">
-            <h3 className="text-xl font-semibold">Blog in Australia</h3>
+            <div className="p-4 bg-white/5 rounded-lg space-y-2">
+              <h3 className="text-xl font-semibold">Blog in Australia</h3>
               <div className="flex flex-col md:flex-row gap-4 items-center">
                 <div className="w-full md:w-1/3">
                   <img 
-                    src="https://placehold.co/400x200" 
+                    src="screenshot\screenshot-aus-blog.sloperiver-1280x720.png" 
                     alt="Blog in Australia" 
                     className="rounded-lg cursor-pointer"
                     onClick={() => window.open('https://aus-blog.sloperiver.com', '_blank')}
@@ -70,17 +103,49 @@ const Portfolio = () => {
                     I manage the blog content through microCMS, 
                     taking advantage of their free tier to keep operational costs at zero.`}
                   </p>
-                  <div className="mt-4 flex flex-wrap gap-2">
-                    <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
-                    <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
-                    <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">Tailwind</span>
-                  </div>
                 </div>
               </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Languages & Frameworks:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
+                  <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
+                  <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">TailwindCSS</span>
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">Next.js</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Deployment & Hosting:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">Vercel</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">CMS:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-cyan-500/20 px-3 py-1 rounded-full text-sm">microCMS</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">others:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">GA4</span>                
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">v0</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">cline(Deepseek v3)</span>
+                </div>
+              </div>                            
             </div>
 
-            <div className="p-4 bg-white/5 rounded-lg">
-            <h3 className="text-xl font-semibold">myglish - remember words</h3>
+            <div className="p-4 bg-white/5 rounded-lg space-y-2">
+              <h3 className="text-xl font-semibold">myglish - remember words</h3>
               <div className="flex flex-col md:flex-row gap-4 items-center">
                 <div className="w-full md:w-1/3">
                   <img 
@@ -100,26 +165,77 @@ const Portfolio = () => {
                     Since these APIs are pay-as-you-go, I implemented user authentication to restrict access to myself. 
                     This project also marked my first foray into backend development.`}
                   </p>
-                  <div className="mt-4 flex flex-wrap gap-2">
-                    <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
-                    <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
-                    <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">Tailwind</span>
-                  </div>
                 </div>
               </div>
-            </div>            
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Languages & Frameworks:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
+                  <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
+                  <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">TailwindCSS</span>
+                  <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">shadcn/ui</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">Node.js</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">Express</span>                  
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Build Tool:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">Vite</span>
+                </div>
+              </div>  
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Deployment & Hosting:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">Netlify</span>
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">Render</span>
+                </div>
+              </div>                          
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">APIs:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">Google Text-to-Speech</span>
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">OpenAI o4-mini</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Authentication:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">Firebase Auth</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Database:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">Firebase Firestore</span>
+                  <span className="bg-gray-500/20 px-3 py-1 rounded-full text-sm">indexedDB</span>
+                </div>
+              </div>
+            </div>
           </div>
         </section>
 
         <section className="bg-white/10 backdrop-blur-sm rounded-lg p-6">
           <h2 className="text-3xl font-bold mb-4">For Learning</h2>
           <div className="space-y-4">
-            <div className="p-4 bg-white/5 rounded-lg">
-            <h3 className="text-xl font-semibold">GIS engineering 1</h3>
+            <div className="p-4 bg-white/5 rounded-lg space-y-2">
+              <h3 className="text-xl font-semibold">GIS engineering 1</h3>
               <div className="flex flex-col md:flex-row gap-4 items-center">
                 <div className="w-full md:w-1/3">
                   <img 
-                    src="https://placehold.co/400x200" 
+                    src="screenshot\screenshot-advanced-map-study.sloperiver-1280x720.png" 
                     alt="GIS engineering 1" 
                     className="rounded-lg cursor-pointer"
                     onClick={() => window.open('https://advanced-map-study.sloperiver.com', '_blank')}
@@ -140,11 +256,40 @@ const Portfolio = () => {
                         現場のプロがわかりやすく教える 位置情報エンジニア養成講座
                       </a>
                   </p>
-                  <div className="mt-4 flex flex-wrap gap-2">
-                    <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">React</span>
-                    <span className="bg-purple-500/20 px-3 py-1 rounded-full text-sm">TypeScript</span>
-                    <span className="bg-green-500/20 px-3 py-1 rounded-full text-sm">Tailwind</span>
-                  </div>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Languages & Frameworks:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-blue-500/20 px-3 py-1 rounded-full text-sm">javascript(Vanilla)</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Build Tool:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-yellow-500/20 px-3 py-1 rounded-full text-sm">Vite</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">Deployment & Hosting:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">GitHub Pages</span>
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">GitHub Actions</span>
+                </div>
+              </div>
+              <div className="flex flex-col md:flex-row gap-4 items-center">
+                <div className="w-full md:w-1/3">
+                  <h6 className="text-base">othres:</h6>
+                </div>
+                <div className="w-full md:w-2/3">
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">python(convert csv→geoJSON)</span>
+                  <span className="bg-indigo-500/20 px-3 py-1 rounded-full text-sm">tippecanoe(convert geoJSON→pbf)</span>
                 </div>
               </div>
             </div>
